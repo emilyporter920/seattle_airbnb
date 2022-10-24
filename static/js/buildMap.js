4d3f48cc54ce73a85354568db6b78801c8be7f4b
Emily Porter <emilyporter920@gmail.com>
2022-10-24 17:01:03 -0400

    fixed files to make JSON dropdown & map work

--- static/js/buildMap.js
+++ static/js/buildMap.js
@@ -1,151 +1,56 @@
-// from data.js
+// default seattle coords
+// centered tableData coords
+const seattle = new google.maps.LatLng(47.61922313, -122.3357037);
 
-const tableData = data;
+// create map, center on Seattle
+const map = new google.maps.Map(document.getElementById("map"), {
+  zoom: 11,
+  center: seattle,
+});
 
+// create empty list for markers 
+markers = []
 
-// get table references
-var tbody = d3.select("tbody");
-
-function buildTable(data) {
-  // First, clear out any existing data
-  tbody.html("");
-
-  // Next, loop through each object in the data
-  // and append a row and cells for each value in the row
-  data.forEach((dataRow) => {
-    // Append a row to the table body
-    let row = tbody.append("tr");
-
-    // Loop through each field in the dataRow and add
-    // each value as a table cell (td)
-    Object.values(dataRow).forEach((val) => {
-      let cell = row.append("td");
-      cell.text(val);
-    });
+// create marker
+function createMarker(row){
+   let marker = new google.maps.Marker({
+    position: {
+      lat: parseFloat(row.latitude), 
+      lng: parseFloat(row.longitude)
+    },
   });
+  marker.content = row
+
+  return marker;
 }
 
-// 1. Create a variable to keep track of all the filters as an object.
-var filters={
-  _count: 0
-};
+// add marker
+function addMarkers(markers){
+  for (let marker of markers){
+    marker.setMap(map);
 
-// 3. Use this function to update the filters. 
-function updateFilters() {
-
-    // 4a. Save the element that was changed as a variable.
-    let changedElement=d3.select(this);
-    // 4b. Save the value that was changed as a variable.
-    let elementValue=changedElement.property("value");
-
-    // Verify that it is saving elementValue
-    console.log(elementValue);
-
-    // 4c. Save the id of the filter that was changed as a variable.
-    let idElement=changedElement.attr("id");
-
-    // Verify that it is saving idElement
-    console.log(idElement);
-  
-    // 5. If a filter value was entered then add that filterId and value
-    // to the filters list. Otherwise, clear that filter from the filters object.
-    if (elementValue) {
-      filters[idElement] = elementValue;
-      filters._count++;
-    }
-    
-    else {
-      delete filters[idElement];
-      filters._count--;
-    }
-
-    // 6. Call function to apply all filters and rebuild the table
-    filterTable();
-  }
-  
-  // 7. Use this function to filter the table when data is entered.
-  function filterTable() {
-  
-    // 8. Set the filtered data to the tableData.
-    let filteredData=tableData;
-    // 9. Loop through all of the filters and keep any data that
-    // matches the filter values (Object.entries() means it will return an array of a given objects [key, value])
-    Object.entries(filters).forEach(([key, value])=> {
-      if (key != "_count"){
-        filteredData=filteredData.filter(row=>row[key]==value);
-      }
-    })
-
-    
-    // if _count is not empty, create marker for each item in filteredData
-    // else clear markers
-    removeMarkers(markers);
-    markers = [];
-
-    if (filters._count > 0){
-      for (let row of filteredData){
-        markers.push(createMarker(row));
-      }
-      addMarkers(markers);
-      console.log(filters);
-
-   
-    }
-    
-
-    // 10. Finally, rebuild the table using the filtered data
-    buildTable(filteredData);
-  }
-  
-  // 2. Attach an event to listen for changes to each filter
-  d3.selectAll("input").on("change", updateFilters);
-  
-  // Build the table when the page loads
-  buildTable(tableData);
-
-  $(document).ready(function(){
-    
-  })
-
-  // Dropdown for Bedrooms
-  var select = '';
-  for (i=1;i<=7;i++){
-    select += '<option val=' + i + '>' + i + '</option>';
-  }
-  $('#selBedroom').html(select);
-
-    // Dropdown for Bedrooms
-    var select = '';
-    for (i=1;i<=5;i++){
-      select += '<option val=' + i + '>' + i + '</option>';
-    }
-    $('#selBathroom').html(select);
-
-  // Dropdown for IDs
-  $("#selID").on("click", function() {
-    let dropdown = $('#selID');
-
-    dropdown.empty();
-    
-    dropdown.append('<option selected="true" disabled></option>');
-    dropdown.prop('selectedIndex', 0);
-    
-    const url = 'https://raw.githubusercontent.com/emilyporter920/seattle_airbnb/main/data.json';
-    
-    // Populate dropdown with list of provinces
-    $.getJSON(url, function (data) {
-      $.each(data, function (key, entry) {
-        dropdown.append($('<option></option>').attr('value', entry.listing_id).text(entry.listing_id));
-      })
+    let contentString = `
+    <h3 class="marker">${marker.content.listing_id}</h3>
+    <p class="marker">Bedrooms: ${marker.content.bedrooms}</p>
+    <p class="marker">Bathrooms: ${marker.content.bathrooms}</p>
+    `
+    const infowindow = new google.maps.InfoWindow({
+      content: contentString,
     });
-
-    $('#selID').val("value")
+    
+    marker.addListener("click", () => {
+      infowindow.open({
+        anchor: marker,
+        map,
+      });
+    });
   
-  });
-
-  // Dropdown for Month
-  var select = '';
-  for (i=1;i<=12;i++){
-    select += '<option val=' + i + '>' + i + '</option>';
   }
-  $('#selMonth').html(select);
+}
+
+// clear markers
+function removeMarkers(markers){
+  for (let marker of markers){
+    marker.setMap(null);
+  }
+}
